24df2ff9485e9ce82b3e195d03f9e0a072776334
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-08-29 16:49:56 +0200

    fix some misspilled words in models.py

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    title = models.CharField(max_lenth=100)
+    title = models.CharField(max_length=100)
     price = models.FloatField()
 
     def __str__(self):
@@ -16,7 +16,7 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     def __str__(self):
-    return self.title
+        return self.title
 
 
 class Order(models.Model):
@@ -28,4 +28,4 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
-    return self.title
+        return self.title
