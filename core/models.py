77dd1988e667fa1c8391e91fa8d3a1930ae9764b
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-08-30 15:13:15 +0200

    add category, label for Item class in models.py

--- core/models.py
+++ core/models.py
@@ -1,12 +1,26 @@
 from django.conf import settings
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Outwear'),
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger'),
+)
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(
+        choices=CATEGORY_CHOICES, max_length=2, default=CATEGORY_CHOICES[0][0])
+    label = models.CharField(choices=LABEL_CHOICES,
+                             max_length=1, default=LABEL_CHOICES[0][0])
 
     def __str__(self):
         return self.title
