eed88928eaa53cec28251eda98a80719fe8287cc
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-08-31 18:16:51 +0200

    add discount_price & description fields to models.py

--- core/models.py
+++ core/models.py
@@ -18,11 +18,13 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(null=True, blank=True)
     category = models.CharField(
         choices=CATEGORY_CHOICES, max_length=2, default=CATEGORY_CHOICES[0][0])
     label = models.CharField(choices=LABEL_CHOICES,
                              max_length=1, default=LABEL_CHOICES[0][0])
     slug = models.SlugField()
+    description = models.TextField(default='Here is the Discription')
 
     def __str__(self):
         return self.title
