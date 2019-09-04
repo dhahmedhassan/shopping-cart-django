76a894f4be915384b319f3b395185106d2ebcd2e
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-09-04 04:14:41 +0200

    wire add_to_cat to urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     HomeView,
     ItemDetailView,
-    checkout
+    checkout,
+    add_to_cart
 )
 
 app_name = 'core'
@@ -11,4 +12,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='item-list'),
     path('checkout/', checkout, name='checkout'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
 ]
